f91430ae855a7ff64525209e8d9dca368f6598df
AndrewLi <13058100289@163.com>
2020-01-19 16:47:22 +0800

    update config.js can make webstorm can find path by @

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,23 @@ const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
+  /*
+   * make webstorm can find path by @
+   */
+  context: path.resolve(__dirname, './'),
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': path.resolve('src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@comp': path.resolve(__dirname, 'src/components'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@plug': path.resolve(__dirname, 'src/plugins'),
+      '@utils': path.resolve(__dirname, 'src/utils')
+    }
+  },
+
   /**
    * You will need to set publicPath if you plan to deploy your site under a sub path,
    * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
